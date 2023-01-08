0367d16ca55fe58e622052a0d1cf2cd41f531976
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-01-08 18:01:30 +0300

    step=2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,14 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
-
+  .version('0.1.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(`filepath1:  ${filepath1}`);
+    console.log(`filepath2:  ${filepath2}`);
+    const options = program.opts();
+    if (options.format) console.log(`format: ${options.format}`);
+  });
+  
   program.parse();
