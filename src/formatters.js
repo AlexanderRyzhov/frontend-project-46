188d675b8ed8605154b934fac07ab985127a8a13
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-01-11 00:28:30 +0300

    src/formatters.js simplify

--- src/formatters.js
+++ src/formatters.js
@@ -1,65 +1,47 @@
 import _ from 'lodash';
 
-const stylish = (data) => {
+const stylish = (data, depth = 0) => {
   const replacer = ' ';
   const spacesCount = 4;
-  const iter = (value, depth) => {
-    if (value === undefined) {
-      return 'undefined';
+  if (data === undefined) {
+    return 'undefined';
+  }
+  if (data === null) {
+    return 'null';
+  }
+  const currentDepth = depth + 1;
+  const prefix = replacer.repeat(spacesCount * currentDepth - 2);
+  const bracePrefix = replacer.repeat(spacesCount * depth);
+  if (!Array.isArray(data)) {
+    if (typeof data === 'object') {
+      const keys = _.sortBy(_.keys(data));
+      const lines = keys.map((key) => `${prefix}  ${key}: ${stylish(data[key], currentDepth)}`);
+      return ['{', ...lines, `${bracePrefix}}`].join('\n');
     }
-    if (value === null) {
-      return 'null';
-    }
-    if (!Array.isArray(value)) {
-      if (typeof value === 'object') {
-        const currentDepth = depth + 1;
-        const prefix = replacer.repeat(spacesCount * (currentDepth) - 2);
-        const bracePrefix = replacer.repeat(spacesCount * (depth));
-        const keys = _.sortBy(_.keys(value));
-        const lines = keys.map((key) => `${prefix}  ${key}: ${iter(value[key], currentDepth)}`);
-        const output = ['{', ...lines, `${bracePrefix}}`].join('\n');
-        return output;
-      }
-      return value.toString();
-    }
-    const currentDepth = depth + 1;
-    const prefix = replacer.repeat(spacesCount * (currentDepth) - 2);
-    const bracePrefix = replacer.repeat(spacesCount * (depth));
-    const lines = value.flatMap(({
-      key, val, oldVal, nodeType, operation,
-    }) => {
-      const valStr = iter(val, currentDepth);
-      let result = [];
-      if (nodeType === 'branch') {
-        if (operation === 'nochange') {
-          result = `${prefix}  ${key}: ${iter(val, currentDepth)}`;
-        } else if (operation === 'add') {
-          result = `${prefix}+ ${key}: ${iter(val, currentDepth)}`;
-        } else if (operation === 'delete') {
-          result = `${prefix}- ${key}: ${iter(val, currentDepth)}`;
-        }
-      } else if (nodeType === 'leaf') {
-        if (operation === 'update') {
-          const oldValStr = iter(oldVal, currentDepth);
-          result = [
-            `${prefix}- ${key}: ${oldValStr}`,
-            `${prefix}+ ${key}: ${valStr}`,
-          ];
-        } else if (operation === 'add') {
-          result = `${prefix}+ ${key}: ${valStr}`;
-        } else if (operation === 'delete') {
-          result = `${prefix}- ${key}: ${valStr}`;
-        } else if (operation === 'nochange') {
-          result = `${prefix}  ${key}: ${valStr}`;
-        }
-      }
+    return data.toString();
+  }
 
-      return result;
-    });
-    const output = ['{', ...lines, `${bracePrefix}}`].join('\n');
-    return output;
-  };
-  return iter(data, 0);
+  const lines = data.flatMap(({
+    key, val, oldVal, operation,
+  }) => {
+    let result = [];
+    const valStr = stylish(val, currentDepth);
+    if (operation === 'update') {
+      const oldValStr = stylish(oldVal, currentDepth);
+      result = [
+        `${prefix}- ${key}: ${oldValStr}`,
+        `${prefix}+ ${key}: ${valStr}`,
+      ];
+    } else if (operation === 'add') {
+      result = `${prefix}+ ${key}: ${valStr}`;
+    } else if (operation === 'delete') {
+      result = `${prefix}- ${key}: ${valStr}`;
+    } else if (operation === 'nochange') {
+      result = `${prefix}  ${key}: ${valStr}`;
+    }
+    return result;
+  });
+  return ['{', ...lines, `${bracePrefix}}`].join('\n');
 };
 
 const formatDiff = (data, formatter = 'stylish') => {
